2d5bba179cb214c9d1ab950b43dbb1a180e956bd
André Bierlein <andre@bierlein.email>
2024-04-05 20:08:22 +0200

    Testing contract calls

--- src/premints/zora_premint_v2/rules.rs
+++ src/premints/zora_premint_v2/rules.rs
@@ -1,13 +1,27 @@
+use alloy::rpc::types::eth::{TransactionInput, TransactionRequest};
 use std::str::FromStr;
 
-use alloy_primitives::Signature;
-use alloy_sol_types::SolStruct;
+use crate::chain_list::CHAINS;
+use alloy_primitives::{Bytes, Signature};
+use alloy_provider::Provider;
+use alloy_sol_macro::sol;
+use alloy_sol_types::{SolCall, SolStruct};
 
-use crate::premints::zora_premint_v2::types::ZoraPremintV2;
+use crate::premints::zora_premint_v2::types::{ZoraPremintV2, PREMINT_FACTORY_ADDR};
 use crate::rules::Evaluation::{Accept, Reject};
 use crate::rules::{Evaluation, Rule, RuleContext};
 use crate::typed_rule;
 use crate::types::{Premint, PremintTypes};
+
+sol! {
+    contract PremintExecutor {
+        function isAuthorizedToCreatePremint(
+            address signer,
+            address premintContractConfigContractAdmin,
+            address contractAddress
+        ) external view returns (bool isAuthorized);
+    }
+}
 
 // create premint v2 rule implementations here
 
@@ -15,10 +29,56 @@ pub async fn is_authorized_to_create_premint(
     premint: ZoraPremintV2,
     context: RuleContext,
 ) -> eyre::Result<Evaluation> {
+    let call = PremintExecutor::isAuthorizedToCreatePremintCall {
+        contractAddress: premint.collection_address,
+        signer: premint.collection.contractAdmin,
+        premintContractConfigContractAdmin: premint.collection.contractAdmin,
+    };
+
+    let chain = CHAINS.get_chain_by_id(premint.chain_id.to());
+
+    match chain {
+        Some(chain) => {
+            let provider = chain.get_rpc(false).await?;
+            let response = provider
+                .call(
+                    &TransactionRequest {
+                        to: Some(PREMINT_FACTORY_ADDR),
+                        input: TransactionInput {
+                            input: Some(Bytes::from(call.abi_encode())),
+                            data: None,
+                        },
+                        ..Default::default()
+                    },
+                    None,
+                )
+                .await;
+
+            match response {
+                Ok(response) => {
+                    let response =
+                        PremintExecutor::isAuthorizedToCreatePremintReturn::from(response.output);
+                    let result = call.abi_decode(&response.output);
+                    if result.is_err() {
+                        return Ok(Reject("Unauthorized to create premint".to_string()));
+                    }
+                    if result.unwrap() {
+                        return Ok(Accept);
+                    }
+                }
+                Err(_) => {
+                    return Ok(Reject("Unauthorized to create premint".to_string()));
+                }
+            }
+        }
+        None => {
+            return Ok(Reject("Chain not supported".to_string()));
+        }
+    }
+
     //   * if contract exists, check if the signer is the contract admin
     //   * if contract does not exist, check if the signer is the proposed contract admin
     //   * this logic exists as a function on the premint executor contract
-    Ok(Accept)
 }
 
 // * signatureIsValid ( this can be performed entirely offline )
